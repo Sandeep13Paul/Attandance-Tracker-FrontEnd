4e632515c7b323f8778bec8826ea1f4b177f3317
Sandeep <sandeep@MacBook-Pro.local>
2026-04-09 12:32:22 +0530

    added correct url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-  (process.env.REACT_APP_API_URL || "http://localhost:8080/api").replace(
+  (process.env.REACT_APP_API_URL || "https://attandance-tracker-backend.onrender.com/api").replace(
     /\/$/,
     ""
   );
